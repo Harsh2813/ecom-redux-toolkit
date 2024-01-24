a0f2468d1ce1a52047d647517b1676d51ea735e4
Harsh2813 <prateektiwari.pk55@gmail.com>
2024-01-24 18:56:57 +0530

    used backend api call with redux by creating action creaters used useEffect

--- src/App.js
+++ src/App.js
@@ -1,16 +1,49 @@
+import { Fragment, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
-import { useSelector } from 'react-redux';
+import Notification from './components/UI/Notification';
+import { sendCartData, fetchCartData } from './store/cart-actions';
+
+let isInitial = true;// notification automatically show ho rha tha page reload hone me without add in cart so aisa na ho only add to cart krne me notification show to uske liye ye isInitial true liye taki page reload me na chale jaise hi page refresh ho false ho jaye ye fir cart me changed state dale agar wo true hua mtlb add to cart ya remove hua to hi hm sendCartData to call karenge
 
 function App() {
+  const dispatch = useDispatch();
   const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);//pura cart jo name h cartSlice ka pura state hi le liye 
+  const notification = useSelector((state) => state.ui.notification);
+
+  useEffect(() => {
+    dispatch(fetchCartData());//jaise hi page reload hoga data fetch ho jayega
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (isInitial) {
+      isInitial = false;//false krke return kr gye jb dependecy change hoga tabhi useEffect call hoga bs
+      return;
+    }
+
+    if (cart.changed) {//jb cart me add ya remove data me hi ye call hoga replace cart wale me ni because cart as dependency h to usme items array and totalCount ke sath changed bhi state h 
+      dispatch(sendCartData(cart));
+    }
+  }, [cart, dispatch]);
 
   return (
-    <Layout>
-      {showCart && <Cart />}
-      <Products />
-    </Layout>
+    <Fragment>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </Fragment>
   );
 }
 
--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -2,15 +2,21 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const cartSlice = createSlice({
   name: "cart",
-  initialState: {
+  initialState: { //jaise hi state update hua fir hm api call krenge iske according in car-actions
     items: [],
     totalQuantity: 0,
+    changed: false,//only add ya remove ke liye h ye changed used in app.js
   },
   reducers: {
+    replaceCart(state, action) {
+      state.totalQuantity = action.payload.totalQuantity;
+      state.items = action.payload.items;
+    },
     addItemToCart(state, action) {
       const newItem = action.payload;
       const existingItem = state.items.find((item) => item.id === newItem.id);
       state.totalQuantity++;
+      state.changed = true;
       if (!existingItem) {
         state.items.push({
           id: newItem.id,
@@ -28,10 +34,10 @@ const cartSlice = createSlice({
       const id = action.payload;
       const existingItem = state.items.find((item) => item.id === id);
       state.totalQuantity--;
-
+      state.changed = true;
       existingItem.quantity--;
       existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
-      state.items = state.items.filter((item) => item.quantity > 0);//mtlb jo bhi particular item hua items array me uski quantity -- hote hote 0 ho jayegi to items array me filter lga diye jiski bhi quantity 0 se badi ho wo bs show ho cart me
+      state.items = state.items.filter((item) => item.quantity > 0); //mtlb jo bhi particular item hua items array me uski quantity -- hote hote 0 ho jayegi to items array me filter lga diye jiski bhi quantity 0 se badi ho wo bs show ho cart me
     },
   },
 });
